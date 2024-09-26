a2a1c6346aac5bf2448cc249095397ba9bed6879
Mark Duppenthaler <mduppes@gmail.com>
2024-09-26 02:31:22 +0000

    limit port

--- setup.py
+++ setup.py
@@ -18,4 +18,15 @@ setuptools.setup(
         "src/_cffi_src/build_vpx.py:ffibuilder",
     ],
     cmdclass={"bdist_wheel": bdist_wheel_abi3},
+    install_requires=[
+        "aioice @ git+ssh://git@github.com/mduppes/aioice#limit_udp_port",
+        "av>=9.0.0,<13.0.0",
+        "cffi>=1.0.0",
+        "cryptography>=42.0.0",
+        'dataclasses; python_version < "3.7"',
+        "google-crc32c>=1.1",
+        "pyee>=9.0.0",
+        "pylibsrtp>=0.10.0",
+        "pyopenssl>=24.0.0",
+    ]
 )
